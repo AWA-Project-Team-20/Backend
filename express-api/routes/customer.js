05acb8d3ca7d09a2b4adb74dcdbad6a1478e5cd1
Faustin Danil <sabakavgope@gmail.com>
2021-11-26 03:18:46 +0200

    Added 2 more functions

--- express-api/routes/customer.js
+++ express-api/routes/customer.js
@@ -47,3 +47,26 @@ router.get("/id/:id", (req, res) => {
 })
 
 
+// DELETE a customer with the requested customer_id
+router.delete("/id/:id", (req, res) => {
+    pool
+  .query(`DELETE FROM customer WHERE customer_id = ${req.params.id}`)
+  .then()
+  .catch(err =>
+    setImmediate(() => {
+      throw err
+    })
+  )
+})
+
+// POST a single customer with the requested login and password
+router.post("/new/:login/:password", (req, res) => {
+    pool
+  .query(`INSERT INTO customer(login, password) VALUES ('${req.params.login}', '${req.params.password}');`)
+  .then()
+  .catch(err =>
+    setImmediate(() => {
+      throw err
+    })
+  )
+})
